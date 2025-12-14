d5d82ef430b4c8d8f4af05fa5c177b142e854ba5
Oleg <oleg_09-95@mail.ru>
2025-12-14 21:39:03 +0300

    Video #11 Начата верстка для соц сети. Собрана разметка на Grid

--- 01-first-project/src/App.jsx
+++ 01-first-project/src/App.jsx
@@ -1,16 +1,46 @@
 import './App.css'
-import Technologies from "./Technologies.jsx";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
 
 function App() {
 
     return (
-        <>
-            <Header />
-            <Technologies />
-            <Footer />
-        </>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://e7.pngegg.com/pngimages/779/61/png-clipart-logo-idea-cute-eagle-leaf-logo-thumbnail.png"
+                    alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <a>Profile</a>
+                <a>Message</a>
+                <a>News</a>
+                <a>Music</a>
+                <a>Settings</a>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://assets.monica.im/tools-web/_next/static/media/mobile_upscale.e93d7497.webp"
+                         alt=""/>
+                </div>
+                <div>
+                    {/*<img src="https://web-tool.org/create-round-image/rectangular-image.jpg" alt=""/>*/}
+                    ava + desc
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
