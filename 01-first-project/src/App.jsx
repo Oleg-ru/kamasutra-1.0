829dcf7af0b4937db0e6b28e7492566e7e4c301d
Oleg <oleg_09-95@mail.ru>
2025-12-14 20:27:14 +0300

    Video #7 Лекция по компонентам

--- 01-first-project/src/App.jsx
+++ 01-first-project/src/App.jsx
@@ -2,16 +2,34 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JS</li>
-          <li>React</li>
-      </ul>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Technologies />
+        </>
+    )
+}
+
+function Header() {
+
+    return (
+        <div>
+            <a>Home</a>
+            <a>News Feed</a>
+            <a>Message</a>
+        </div>
+    )
+}
+
+function Technologies() {
+    return (
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+    );
 }
 
 export default App
