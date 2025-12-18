eb051111b5b35778b496a5974d227181f650d559
Oleg <oleg_09-95@mail.ru>
2025-12-18 20:50:26 +0300

    Video #21 Добавлен контент на страницу Dialogs

--- 01-first-project/src/components/Dialogs/Dialogs.jsx
+++ 01-first-project/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,24 @@ import styles from './Dialogs.module.css'
 
 function Dialogs(props) {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    Vladimir
+                </div>
+                <div className={styles.dialog}>
+                    Anton
+                </div>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Anastasia
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>Hello!!!</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>Allloooooo</div>
+            </div>
         </div>
     );
 }
