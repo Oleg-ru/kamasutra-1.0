f4e5979dc32d07a7386929cf35334725ec731662
Oleg <oleg_09-95@mail.ru>
2025-12-18 21:19:17 +0300

    Video #21 Обернуты диалоги в NavLink. Почитал доку, что exact стал по умолчанию.

--- 01-first-project/src/components/Dialogs/Dialogs.jsx
+++ 01-first-project/src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Dialogs.module.css'
+import {NavLink} from "react-router";
 
 function Dialogs(props) {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
                 <div className={styles.dialog}>
-                    Vladimir
+                    <NavLink to='/dialogs/1'>Vladimir</NavLink>
                 </div>
                 <div className={styles.dialog}>
-                    Anton
+                    <NavLink to='/dialogs/2'>Anton</NavLink>
                 </div>
                 <div className={styles.dialog + ' ' + styles.active}>
-                    Anastasia
+                    <NavLink to='/dialogs/3'>Anastasia</NavLink>
                 </div>
             </div>
             <div className={styles.messages}>
--- 01-first-project/src/components/Navbar/Navbar.jsx
+++ 01-first-project/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     return (
         <nav className={styles.nav}>
             <NavLink to="/profile" className={navStyle}>Profile</NavLink>
-            <NavLink to="/dilaogs" className={navStyle}>Message</NavLink>
+            <NavLink to="/dialogs/*" className={navStyle}>Message</NavLink>
             <NavLink to="/news" className={navStyle}>News</NavLink>
             <NavLink to="/music" className={navStyle}>Music</NavLink>
             <NavLink to="/settings" className={navStyle}>Settings</NavLink>
