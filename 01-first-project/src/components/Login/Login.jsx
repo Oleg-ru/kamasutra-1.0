7adf6c8a90a8231b649d6a64c322f03ec0e08f1f
Oleg <oleg_09-95@mail.ru>
2026-02-13 22:32:04 +0300

    Video #75 Подключены final-form и react-final-form (улучшенная версия redux-form) Создана базовая верстка форма для логина.

--- 01-first-project/src/components/Login/Login.jsx
+++ 01-first-project/src/components/Login/Login.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
 import styles from './Login.module.css'
+import {Form, Field} from 'react-final-form'
 
 function Login(props) {
     return (
-        <div>LOGIN</div>
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
     );
 }
+
+function LoginForm(props) {
+
+    const onSubmit = (data) => {
+        console.log(data)
+        console.log("Submited")
+    };
+
+    return (
+        <Form onSubmit={onSubmit}
+              render={({handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+                      <div>
+                          <label>
+                              Login
+                              <Field name="login" component="input" placeholder="Login"/>
+                          </label>
+                      </div>
+                      <div>
+                          <label>
+                              Password
+                              <Field name="password" component="input" type="password" placeholder="Password"/>
+                          </label>
+                      </div>
+                      <div>
+                          <label>
+                              Remember me
+                              <Field name="rememberMe" component="input" type="checkbox" />
+                          </label>
+                      </div>
+                      <button type="submit">Login</button>
+                  </form>
+              )}
+        />
+    );
+}
+
+/*
+<form className={styles.form}>
+            <div>
+                <input placeholder="Login" type="text"/>
+                <input placeholder="Password" type="password"/>
+                <div>
+                    <label>
+                        <span>It`s me</span>
+                        <input type="checkbox"/>
+                    </label>
+                </div>
+                <button>Login</button>
+            </div>
+        </form>
+ */
+
 
 export default Login;
