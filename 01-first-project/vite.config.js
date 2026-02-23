6faab191746c046a15c424897d2b6c212ea77fca
Oleg <oleg_09-95@mail.ru>
2026-02-23 18:56:38 +0300

    Video #95 add gh-pages

--- 01-first-project/vite.config.js
+++ 01-first-project/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kamasutra-1.0/',
   test: {
     environment: 'jsdom',
     globals: true,
